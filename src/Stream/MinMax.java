fe3824b9e55ecb08baf6242ae6c1553290bfed7c
Milena Marcele <milenamarcele777@gmail.com>
2020-09-18 17:14:58 -0300

    exercitando conceito de min/max, utilizando classe alunos para imprimir a maior nota e a menor nota

---
+++ src/Stream/MinMax.java
@@ -0,0 +1,27 @@
+package Stream;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+
+public class MinMax {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno ("Luis", 7.4);
+        Aluno a2 = new Aluno ("Marco", 5.8);
+        Aluno a3 = new Aluno ("Maria", 9.4);
+        Aluno a4 = new Aluno ("Enzo", 10);
+
+        List<Aluno> alunos = Arrays.asList (a1, a2, a3, a4);
+
+        Comparator<Aluno> melhorNota = (aluno1, aluno2) -> {
+            if(aluno1.nota > aluno2.nota) return 1;
+            if(aluno1.nota < aluno2.nota) return  -1;
+            return 0;
+        };
+
+        System.out.println (alunos.stream( ).max (melhorNota).get () );
+        System.out.println (alunos.stream( ).min (melhorNota).get () );
+
+    }
+}
