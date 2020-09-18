4af0743ad34dc4c026418288f3d9bf7345fe29f6
Milena Marcele <milenamarcele777@gmail.com>
2020-09-18 16:27:35 -0300

    Aplicando map e exemplificando com marcas de carro

---
+++ src/Stream/Map.java
@@ -0,0 +1,24 @@
+package Stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
+public class Map {
+    public static void main(String[] args) {
+
+        Consumer<String> print = System.out::println;
+        List<String> marcas = Arrays.asList ("BMW", "Honda", "Audi");
+
+        marcas.stream ().map (m -> m.toUpperCase ( )).forEach (print);
+
+        UnaryOperator<String> maiuscula = n -> n.toUpperCase ();
+        UnaryOperator<String> primeiraLetra = n -> n.charAt (0) + "";
+        //  UnaryOperator<String> grito = n -> n + "!!!";
+
+        System.out.println ("\n Usando composição" );
+        marcas.stream ().map (maiuscula).map (primeiraLetra).map (Utilitarios::grito).forEach (print);
+
+    }
+}
