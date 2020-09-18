80cd83019085140cf36c77496280025a8ab0a7d0
Milena Marcele <milenamarcele777@gmail.com>
2020-09-18 16:29:33 -0300

    Criada classe que servira como base para desafiofilter

---
+++ src/Stream/Apresentacoes.java
@@ -0,0 +1,15 @@
+package Stream;
+
+public class Apresentacoes {
+    final String tema;
+    final double nota;
+    final boolean boaApresentacao;
+
+    public Apresentacoes(String tema, double nota, boolean boaApresentacao) {
+        this.tema = tema;
+        this.nota = nota;
+        this.boaApresentacao = boaApresentacao;
+    }
+
+}
+
